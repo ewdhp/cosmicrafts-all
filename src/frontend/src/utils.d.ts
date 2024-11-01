9c28617c51312cb062af73f6a5857a57f5d62bfb
ewdhp <ewdhp@outlook.com>
2024-11-01 16:51:04 -0600

    removed frontend and unused backend

--- src/frontend/src/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export function customStringify(obj: any): string;
-export function customParse(str: string): any;
-export function parseMetadata(metadata: any): any;
--- src/frontend/src/views/dashboard/DBView.vue
+++
@@ -1,8 +0,0 @@
-<template>
- <div>
-   <h2>Motoko backend preview</h2>
- </div>
-</template>
-
-<script>
-</script>
